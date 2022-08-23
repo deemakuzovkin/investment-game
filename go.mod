bfeaad66a4230f8ea2c519e639f9d05174398a4b
deemak <deema_k@mail.ru>
2022-08-23 20:48:25 +0300

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/deemakuzovkin/investment-game
+
+go 1.18
---
+++ pkg/utils/environment.go
@@ -0,0 +1,11 @@
+package utils
+
+import "os"
+
+func GetEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value != `` {
+		return fallback
+	}
+	return value
+}
